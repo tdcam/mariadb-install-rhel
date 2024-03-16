a10bfaec049f48d4cd99e48072ccaf55b1c6625f
System Admin <sysadmin@p16s.sparksoftcorp.io>
2024-03-15 19:37:15 -0700

    added sql script

---
+++ wordpress.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE wordpress;
+CREATE USER "wpadmin"@"host183.virtual.lan" IDENTIFIED BY "wordpress";
+GRANT ALL PRIVILEGES ON "wordpress.*" TO "wpadmin"@"host183.virtual.lan";
+FLUSH PRIVILEGES;
