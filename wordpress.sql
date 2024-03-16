b47af12478391e657237f8ade5edb6ed2e226ea6
System Admin <sysadmin@p16s.sparksoftcorp.io>
2024-03-15 19:44:14 -0700

    fixed sql typo

--- wordpress.sql
+++ wordpress.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE wordpress;
 CREATE USER "wpadmin"@"host183.virtual.lan" IDENTIFIED BY "wordpress";
-GRANT ALL PRIVILEGES ON "wordpress.*" TO "wpadmin"@"host183.virtual.lan";
+GRANT ALL PRIVILEGES ON wordpress.* TO "wpadmin"@"host183.virtual.lan";
 FLUSH PRIVILEGES;
+
